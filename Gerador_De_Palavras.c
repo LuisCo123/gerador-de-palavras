0490ecd740e2d05a402c6dccb83c091ae0dda682
luismal123 <51391999+luismal123@users.noreply.github.com>
2019-09-15 22:26:46 -0300

    Add files via upload

--- Gerador_De_Palavras.c
+++ Gerador_De_Palavras.c
@@ -5,32 +5,35 @@
 
 int dissilaba(char alfabeto[], char vogais[]);
 int trissilaba(char alfabeto[], char vogais[]);
-void polissilaba (char alfabeto[], char vogais[]);
+int polissilaba (char alfabeto[], char vogais[]);
 int validarpalavra (char palavra[], int quantidade);
 
 
 void limpapalavraaux (char palavraaux[]){
 	int i;
 	int aux = strlen(palavraaux);
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < aux; i++)
 		palavraaux[i] = '\0';
 		
 }
 int validarpalavra (char palavra[], int quantidade){
 		FILE *p;
-
 		if(quantidade == 5)
 			p = fopen("Dicionario\\Dissilaba4.txt","r");
-		
 		else if(quantidade == 6)
 			p = fopen("Dicionario\\Dissilaba5.txt","r");
 		else if(quantidade == 7)
 			p = fopen("Dicionario\\Trissilaba6.txt","r");
 		else if(quantidade == 8)
 			p = fopen("Dicionario\\Trissilaba7.txt","r");
-
+		else if (quantidade == 9)
+			p = fopen("Dicionario\\Polissilaba8.txt", "r");
+		else if (quantidade == 11)
+			p = fopen("Dicionario\\Polissilaba10.txt", "r");
+		else if (quantidade == 13)
+			p = fopen("Dicionario\\Polissilaba12.txt", "r");
 		if(!p){
-			printf("arquivo nao abriu");
+			printf("falha ao abrir o arquivo");
 			return 0;
 		}
 		
@@ -46,34 +49,11 @@ int validarpalavra (char palavra[], int quantidade){
 		fclose(p);
 		return 0;
 }
-/*void validarpalavra (char palavra[], char letra){
-	FILE *p;
-	char palavraaux[20];
-	int i = 0;
-	int retorno;
-	switch (letra){
-		case 'a':
-			p = fopen ("iniA.txt", "r");
-			char aux;
-			while ((aux = fgetc(p)) != EOF){
-				if (aux == ';'){
-					retorno = strcmp (palavra, palavraaux);
-					if (retorno == 0)
-						puts (palavra);
-					limpapalavraaux (palavraaux);
-					i = 0;
-				}
-				palavraaux[i] = aux;
-				i++;
-			}
-	}
-}*/
+
 int main(){
 	char alfabeto[26];
     char vogais[5] = {'a','e','i','o','u'};
     int num, k = 0, qtd, i=0, j=0,ret=0;
-    //char palavra[4] = "aba";
-    //validarpalavra (palavra, palavra[0]);
     srand(time(NULL));
 
     for (i = 0; i < 26; i++){
@@ -84,23 +64,20 @@ int main(){
     scanf("%d", &k);
    	
    while(j<k){
-   	 qtd = rand()%2;
+   	 qtd = 0;//utilizar (rand()%3) caso queira gerar as 3 possibilidades de uma so vez;
 	   switch(qtd){
 	   				case 0 : 
 	   						ret = dissilaba(alfabeto, vogais);
 	   						j+=ret;
-	   						//printf ("\n");
 	   					break;
 
 	   				case 1 : 
 	   						ret = trissilaba(alfabeto, vogais);
-	   						//printf ("\n");
 	   						j+= ret;
 	   					break;
 	   				case 2:
-	   						polissilaba(alfabeto, vogais);
-	   						//printf ("\n");
-	   						j++;
+	   						ret = polissilaba(alfabeto, vogais);
+	   						j+=ret;
 	   					break;
 	   }
 	}
@@ -476,27 +453,217 @@ int trissilaba(char alfabeto[], char vogais[]){
 					}
 				}
 }
-void polissilaba (char alfabeto[], char vogais[]){
+int polissilaba (char alfabeto[], char vogais[]){
 
- 	int qtd = 0;
-		
-	qtd = rand() % 3;
+ 	char palavra[13];
+	int i, qtd = 0;
+	int ret=0;
+	
+	for(i=0; i < 13; i++)
+		palavra[i]='\0';
+				 	
+	qtd = rand() % 11;
 	switch(qtd){
-		case 0:	
-			//printf ("\t\tentrou\n");
-			dissilaba(alfabeto, vogais);
-			dissilaba(alfabeto, vogais);
-		break;
-		
-		case 1:
-			//printf ("\tentrou\n");
-			trissilaba(alfabeto, vogais);
-			dissilaba(alfabeto, vogais);
-		break;
-		
-		case 2:
-			//printf ("entrou\n");
-			dissilaba(alfabeto, vogais);
-			trissilaba(alfabeto, vogais);
+				case 0:
+						palavra[0]=vogais[rand() % 5];
+						palavra[1]=alfabeto[rand() % 26];
+						palavra[2]=vogais[rand() % 5];
+						palavra[3]=alfabeto[rand() % 26];
+						palavra[4]=alfabeto[rand() % 26];
+						palavra[5]=vogais[rand() % 5];
+						palavra[6]=alfabeto[rand() % 26];
+						palavra[7]=vogais[rand() % 5];
+						ret = validarpalavra(palavra, 9);
+				 		if(ret==1){
+				 			printf("%s\n", palavra);
+				 			return ret;
+				 		}
+						else return 0;
+					break;
+				case 1:
+						palavra[0]=vogais[rand() % 6];
+						palavra[1]=alfabeto[rand() % 26];
+						palavra[2]=vogais[rand() % 6];
+						palavra[3]=alfabeto[rand() % 26];
+						palavra[4]=vogais[rand() % 5];
+						palavra[5]=alfabeto[rand() % 26];
+						palavra[6]=alfabeto[rand() % 26];
+						palavra[7]=vogais[rand() % 5];
+						ret = validarpalavra(palavra, 9);
+				 		if(ret==1){
+				 			printf("%s\n", palavra);
+				 			return ret;
+				 		}
+						else return 0;
+					break;
+					
+				case 2:
+						palavra[0]=vogais[rand() % 5];
+						palavra[1]=alfabeto[rand() % 26];
+						palavra[2]=alfabeto[rand() % 26];
+						palavra[3]=vogais[rand() % 5];
+						palavra[4]=vogais[rand() % 5];
+						palavra[5]=alfabeto[rand() % 26];
+						palavra[6]=vogais[rand() % 5];
+						palavra[7]=alfabeto[rand() % 26];
+						ret = validarpalavra(palavra, 9);
+				 		if(ret==1){
+				 			printf("%s\n", palavra);
+				 			return ret;
+				 		}
+						else return 0;
+					break;
+					
+				case 3:
+						palavra[0]=vogais[rand() % 4];
+						palavra[1]=alfabeto[rand() % 26];
+						palavra[2]=vogais[rand() % 4];
+						palavra[3]=alfabeto[rand() % 26];
+						palavra[4]=vogais[rand() % 4];
+						palavra[5]=alfabeto[rand() % 26];
+						palavra[6]=vogais[rand() % 4];
+						palavra[7]=alfabeto[rand() % 26];
+				 		ret = validarpalavra(palavra, 9);
+				 		if(ret==1){
+				 			printf("%s\n", palavra);
+				 			return ret;
+				 		}
+						else return 0;
+					break;
+					
+				case 4:
+						palavra[0]=vogais[rand() % 5];
+						palavra[1]=alfabeto[rand() % 26];
+						palavra[2]=vogais[rand() % 5];
+						palavra[3]=vogais[rand() % 5];
+						palavra[4]=alfabeto[rand() % 26];
+						palavra[5]=alfabeto[rand() % 26];
+						palavra[6]=vogais[rand() % 5];
+						palavra[7]=vogais[rand() % 5];
+						palavra[8]=alfabeto[rand() % 26];
+						palavra[9]=vogais[rand() % 5];
+						ret = validarpalavra(palavra, 11);
+						if(ret==1){
+			 			printf("%s\n", palavra);
+			 			return ret;
+			 			}
+			 			else return 0;
+					break;
+					
+				case 5://açoitasses
+				 		palavra[0]=vogais[rand() % 5];
+				 		palavra[1]=alfabeto[rand() % 26];
+				 		palavra[2]=vogais[rand() % 5];
+				 		palavra[3]=vogais[rand() % 5];
+				 		palavra[4]=alfabeto[rand() % 26];
+				 		palavra[5]=vogais[rand() % 5];
+				 		palavra[6]=alfabeto[rand() % 26];
+				 		palavra[7]=alfabeto[rand() % 26];
+				 		palavra[8]=vogais[rand() % 5];
+				 		palavra[9]=alfabeto[rand() % 26];
+				 		ret = validarpalavra(palavra, 11);
+				 		if(ret==1){
+				 			printf("%s\n", palavra);
+				 			return ret;
+				 		}
+				 		else return 0;
+				 	break;
+				 	
+				 case 6:
+						palavra[0]=vogais[rand() % 5];
+				 		palavra[1]=alfabeto[rand() % 26];
+				 		palavra[2]=alfabeto[rand() % 26];
+						palavra[3]=vogais[rand() % 5];
+				 		palavra[4]=alfabeto[rand() % 26];
+						palavra[5]=vogais[rand() % 5];
+						palavra[6]=vogais[rand() % 5];
+				 		palavra[7]=alfabeto[rand() % 26];
+				 		palavra[8]=alfabeto[rand() % 26];
+						palavra[9]=vogais[rand() % 5];
+						ret = validarpalavra(palavra, 11);
+						if(ret==1){
+			 			printf("%s\n", palavra);
+			 			return ret;
+			 			}
+			 			else return 0;
+			 		break;
+			 	case 7:
+						palavra[0]=vogais[rand() % 5];
+				 		palavra[1]=alfabeto[rand() % 26];
+						palavra[2]=vogais[rand() % 5];
+				 		palavra[3]=alfabeto[rand() % 26];
+				 		palavra[4]=alfabeto[rand() % 26];
+						palavra[5]=vogais[rand() % 5];
+				 		palavra[6]=alfabeto[rand() % 26];
+						palavra[7]=vogais[rand() % 5];
+				 		palavra[8]=alfabeto[rand() % 26];
+						palavra[9]=vogais[rand() % 5];
+						ret = validarpalavra(palavra, 11);
+						if(ret==1){
+			 			printf("%s\n", palavra);
+			 			return ret;
+			 			}
+			 			else return 0;
+			 		break;
+			 		
+			 	case 8:
+						palavra[0]=vogais[rand() % 5];
+				 		palavra[1]=alfabeto[rand() % 26];
+						palavra[2]=vogais[rand() % 5];
+				 		palavra[3]=alfabeto[rand() % 26];
+						palavra[4]=vogais[rand() % 5];
+				 		palavra[5]=alfabeto[rand() % 26];
+						palavra[6]=vogais[rand() % 5];
+				 		palavra[7]=alfabeto[rand() % 26];
+						palavra[8]=vogais[rand() % 5];
+						palavra[9]=alfabeto[rand() % 26];
+						ret = validarpalavra(palavra, 11);
+						if(ret==1){
+			 			printf("%s\n", palavra);
+			 			return ret;
+			 			}
+			 			else return 0;
+			 		break;
+			 	case 9:
+						palavra[0]=vogais[rand() % 5];
+				 		palavra[1]=alfabeto[rand() % 26];
+				 		palavra[2]=alfabeto[rand() % 26];
+						palavra[3]=vogais[rand() % 5];
+						palavra[4]=vogais[rand() % 5];
+						palavra[5]=vogais[rand() % 5];
+				 		palavra[6]=alfabeto[rand() % 26];
+						palavra[7]=vogais[rand() % 5];
+				 		palavra[8]=alfabeto[rand() % 26];
+						palavra[9]=vogais[rand() % 5];
+						palavra[10]=vogais[rand() % 5];
+						palavra[11]=alfabeto[rand() % 26];
+						ret = validarpalavra(palavra, 13);
+						if(ret==1){
+			 			printf("%s\n", palavra);
+			 			return ret;
+			 			}
+			 			else return 0;
+			 		break;
+			 	case 10:
+						palavra[0]=vogais[rand() % 5];
+				 		palavra[1]=alfabeto[rand() % 26];
+				 		palavra[2]=alfabeto[rand() % 26];
+						palavra[3]=vogais[rand() % 5];
+						palavra[4]=vogais[rand() % 5];
+				 		palavra[5]=alfabeto[rand() % 26];
+						palavra[6]=vogais[rand() % 5];
+				 		palavra[7]=alfabeto[rand() % 26];
+						palavra[8]=vogais[rand() % 5];
+				 		palavra[9]=alfabeto[rand() % 26];
+						palavra[10]=vogais[rand() % 5];
+						palavra[11]=vogais[rand() % 5];
+						ret = validarpalavra(palavra, 13);
+						if(ret==1){
+			 			printf("%s\n", palavra);
+			 			return ret;
+			 			}
+			 			else return 0;
+			 		break;
+			 	
 	}
 }
